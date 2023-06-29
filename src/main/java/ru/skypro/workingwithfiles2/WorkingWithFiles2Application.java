d632c85137ed4f9361b3dcc606cb77df78950d8d
Anton <belix888@yandex.ru>
2023-06-29 23:20:32 +0500

    WorkingWithFiles - исходный код.

---
+++ src/main/java/ru/skypro/workingwithfiles2/WorkingWithFiles2Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.workingwithfiles2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithFiles2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingWithFiles2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/workingwithfiles2/WorkingWithFiles2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.workingwithfiles2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingWithFiles2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
